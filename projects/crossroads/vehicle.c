0f61cac6b22278263764cc648bd626ac67b59e27
ahn_linux_virtualmachine <ahn.avocado@gmail.com>
2024-06-11 05:09:08 -0700

    solved deadlock but it doesn't move

--- projects/crossroads/vehicle.c
+++ projects/crossroads/vehicle.c
@@ -11,45 +11,219 @@
 const struct position vehicle_path[4][4][12] = {
 	/* from A */ {
 		/* to A */
-		{{4,0},{4,1},{4,2},{4,3},{4,4},{3,4},{2,4},{2,3},{2,2},{2,1},{2,0},{-1,-1},},
+		{
+			{4, 0},
+			{4, 1},
+			{4, 2},
+			{4, 3},
+			{4, 4},
+			{3, 4},
+			{2, 4},
+			{2, 3},
+			{2, 2},
+			{2, 1},
+			{2, 0},
+			{-1, -1},
+		},
 		/* to B */
-		{{4,0},{4,1},{4,2},{5,2},{6,2},{-1,-1},},
+		{
+			{4, 0},
+			{4, 1},
+			{4, 2},
+			{5, 2},
+			{6, 2},
+			{-1, -1},
+		},
 		/* to C */
-		{{4,0},{4,1},{4,2},{4,3},{4,4},{4,5},{4,6},{-1,-1},},
+		{
+			{4, 0},
+			{4, 1},
+			{4, 2},
+			{4, 3},
+			{4, 4},
+			{4, 5},
+			{4, 6},
+			{-1, -1},
+		},
 		/* to D */
-		{{4,0},{4,1},{4,2},{4,3},{4,4},{3,4},{2,4},{1,4},{0,4},{-1,-1},}
-	},
-	/* from B */ {
-		/* to A */
-		{{6,4},{5,4},{4,4},{3,4},{2,4},{2,3},{2,2},{2,1},{2,0},{-1,-1},},
-		/* to B */
-		{{6,4},{5,4},{4,4},{3,4},{2,4},{2,3},{2,2},{3,2},{4,2},{5,2},{6,2},{-1,-1},},
-		/* to C */
-		{{6,4},{5,4},{4,4},{4,5},{4,6},{-1,-1},},
-		/* to D */
-		{{6,4},{5,4},{4,4},{3,4},{2,4},{1,4},{0,4},{-1,-1},}
-	},
-	/* from C */ {
-		/* to A */
-		{{2,6},{2,5},{2,4},{2,3},{2,2},{2,1},{2,0},{-1,-1},},
-		/* to B */
-		{{2,6},{2,5},{2,4},{2,3},{2,2},{3,2},{4,2},{5,2},{6,2},{-1,-1},},
-		/* to C */
-		{{2,6},{2,5},{2,4},{2,3},{2,2},{3,2},{4,2},{4,3},{4,4},{4,5},{4,6},{-1,-1},},
-		/* to D */
-		{{2,6},{2,5},{2,4},{1,4},{0,4},{-1,-1},}
-	},
-	/* from D */ {
-		/* to A */
-		{{0,2},{1,2},{2,2},{2,1},{2,0},{-1,-1},},
-		/* to B */
-		{{0,2},{1,2},{2,2},{3,2},{4,2},{5,2},{6,2},{-1,-1},},
-		/* to C */
-		{{0,2},{1,2},{2,2},{3,2},{4,2},{4,3},{4,4},{4,5},{4,6},{-1,-1},},
-		/* to D */
-		{{0,2},{1,2},{2,2},{3,2},{4,2},{4,3},{4,4},{3,4},{2,4},{1,4},{0,4},{-1,-1},}
+		{
+			{4, 0},
+			{4, 1},
+			{4, 2},
+			{4, 3},
+			{4, 4},
+			{3, 4},
+			{2, 4},
+			{1, 4},
+			{0, 4},
+			{-1, -1},
+		}},
+	/* from B */ {/* to A */
+				  {
+					  {6, 4},
+					  {5, 4},
+					  {4, 4},
+					  {3, 4},
+					  {2, 4},
+					  {2, 3},
+					  {2, 2},
+					  {2, 1},
+					  {2, 0},
+					  {-1, -1},
+				  },
+				  /* to B */
+				  {
+					  {6, 4},
+					  {5, 4},
+					  {4, 4},
+					  {3, 4},
+					  {2, 4},
+					  {2, 3},
+					  {2, 2},
+					  {3, 2},
+					  {4, 2},
+					  {5, 2},
+					  {6, 2},
+					  {-1, -1},
+				  },
+				  /* to C */
+				  {
+					  {6, 4},
+					  {5, 4},
+					  {4, 4},
+					  {4, 5},
+					  {4, 6},
+					  {-1, -1},
+				  },
+				  /* to D */
+				  {
+					  {6, 4},
+					  {5, 4},
+					  {4, 4},
+					  {3, 4},
+					  {2, 4},
+					  {1, 4},
+					  {0, 4},
+					  {-1, -1},
+				  }},
+	/* from C */ {/* to A */
+				  {
+					  {2, 6},
+					  {2, 5},
+					  {2, 4},
+					  {2, 3},
+					  {2, 2},
+					  {2, 1},
+					  {2, 0},
+					  {-1, -1},
+				  },
+				  /* to B */
+				  {
+					  {2, 6},
+					  {2, 5},
+					  {2, 4},
+					  {2, 3},
+					  {2, 2},
+					  {3, 2},
+					  {4, 2},
+					  {5, 2},
+					  {6, 2},
+					  {-1, -1},
+				  },
+				  /* to C */
+				  {
+					  {2, 6},
+					  {2, 5},
+					  {2, 4},
+					  {2, 3},
+					  {2, 2},
+					  {3, 2},
+					  {4, 2},
+					  {4, 3},
+					  {4, 4},
+					  {4, 5},
+					  {4, 6},
+					  {-1, -1},
+				  },
+				  /* to D */
+				  {
+					  {2, 6},
+					  {2, 5},
+					  {2, 4},
+					  {1, 4},
+					  {0, 4},
+					  {-1, -1},
+				  }},
+	/* from D */ {/* to A */
+				  {
+					  {0, 2},
+					  {1, 2},
+					  {2, 2},
+					  {2, 1},
+					  {2, 0},
+					  {-1, -1},
+				  },
+				  /* to B */
+				  {
+					  {0, 2},
+					  {1, 2},
+					  {2, 2},
+					  {3, 2},
+					  {4, 2},
+					  {5, 2},
+					  {6, 2},
+					  {-1, -1},
+				  },
+				  /* to C */
+				  {
+					  {0, 2},
+					  {1, 2},
+					  {2, 2},
+					  {3, 2},
+					  {4, 2},
+					  {4, 3},
+					  {4, 4},
+					  {4, 5},
+					  {4, 6},
+					  {-1, -1},
+				  },
+				  /* to D */
+				  {
+					  {0, 2},
+					  {1, 2},
+					  {2, 2},
+					  {3, 2},
+					  {4, 2},
+					  {4, 3},
+					  {4, 4},
+					  {3, 4},
+					  {2, 4},
+					  {1, 4},
+					  {0, 4},
+					  {-1, -1},
+				  }}};
+int vehicle_cnt;
+
+int checkVehicleInCrossroad()
+{
+	int vehicleInCroosroad = 0;
+	struct vehicle_info *vi;
+
+	for (int i = 2; i <= 4; i++)
+	{
+		for (int j = 2; j <= 4; j++)
+		{
+			printf("%d-----%d\n", i, j);
+			// if(lock_try_acquire(&vi->map_locks[i][j])){
+			// 	vehicleInCroosroad++;
+
+			// }
+			// printf("---------%B\n",lock_try_acquire(&vi->map_locks[i][j]));
+		}
 	}
-};
+	// printf(vehicleInCroosroad);
+	return vehicleInCroosroad;
+}
 
 static int is_position_outside(struct position pos)
 {
@@ -60,13 +234,16 @@ static int is_position_outside(struct position pos)
 static int try_move(int start, int dest, int step, struct vehicle_info *vi)
 {
 	struct position pos_cur, pos_next;
+	int vehicleInCroosroad = 0;
 
 	pos_next = vehicle_path[start][dest][step];
 	pos_cur = vi->position;
 
-	if (vi->state == VEHICLE_STATUS_RUNNING) {
+	if (vi->state == VEHICLE_STATUS_RUNNING)
+	{
 		/* check termination */
-		if (is_position_outside(pos_next)) {
+		if (is_position_outside(pos_next))
+		{
 			/* actual move */
 			vi->position.row = vi->position.col = -1;
 			/* release previous */
@@ -74,23 +251,50 @@ static int try_move(int start, int dest, int step, struct vehicle_info *vi)
 			return 0;
 		}
 	}
-
+	vehicleInCroosroad = 0;
+	for (int i = 2; i <= 4; i++){
+		for (int j = 2; j <= 4; j++){
+			if (i == vi->position.row && j == vi->position.col){
+			}
+			else
+			{
+				if (lock_try_acquire(&vi->map_locks[i][j])){
+					lock_release(&vi->map_locks[i][j]);
+				}
+				else{
+					vehicleInCroosroad++;
+				}
+			}
+		}
+	}
+	printf("%d||||||||||%d\n",vi->state,vehicleInCroosroad);
+	if (vehicleInCroosroad >= 3)
+	{
+		return -1;
+	}
+	
+	
 	/* lock next position */
 	lock_acquire(&vi->map_locks[pos_next.row][pos_next.col]);
-	if (vi->state == VEHICLE_STATUS_READY) {
+	if (vi->state == VEHICLE_STATUS_READY)
+	{
 		/* start this vehicle */
 		vi->state = VEHICLE_STATUS_RUNNING;
-	} else {
+	}
+	else
+	{
 		/* release current position */
 		lock_release(&vi->map_locks[pos_cur.row][pos_cur.col]);
 	}
 	/* update position */
 	vi->position = pos_next;
-	
+
 	return 1;
 }
 
-void init_on_mainthread(int thread_cnt){
+void init_on_mainthread(int thread_cnt)
+{
+	vehicle_cnt = thread_cnt;
 	/* Called once before spawning threads */
 }
 
@@ -108,21 +312,28 @@ void vehicle_loop(void *_vi)
 	vi->state = VEHICLE_STATUS_READY;
 
 	step = 0;
-	while (1) {
+	while (1)
+	{
 		/* vehicle main code */
 		res = try_move(start, dest, step, vi);
-		if (res == 1) {
+		if (res == 1)
+		{
 			step++;
 		}
 
-		/* termination condition. */ 
-		if (res == 0) {
+		/* termination condition. */
+		if (res == 0)
+		{
+			break;
+		}
+		if (res == -1)
+		{
 			break;
 		}
 
 		/* unitstep change! */
 		unitstep_changed();
-	}	
+	}
 
 	/* status transition must happen before sema_up */
 	vi->state = VEHICLE_STATUS_FINISHED;
